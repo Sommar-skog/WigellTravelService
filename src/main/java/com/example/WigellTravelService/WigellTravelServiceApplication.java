c6376d64cbb740c9cb60b71edcea2bcaa08463fd
Sommar-skog <amandaaronsson@hotmail.com>
2025-09-17 10:50:30 +0200

    Initial commit

---
+++ src/main/java/com/example/WigellTravelService/WigellTravelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.WigellTravelService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WigellTravelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WigellTravelServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WigellTravelService/WigellTravelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WigellTravelService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigellTravelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
