ac8e5405443d66cd515e54fd7363232e7d9f538c
Sommar-skog <amandaaronsson@hotmail.com>
2025-09-17 13:36:08 +0200

    added CustomerController

---
+++ src/main/java/com/example/WigellTravelService/controllers/TravelCustomerController.java
@@ -0,0 +1,27 @@
+package com.example.WigellTravelService.controllers;
+
+import com.example.WigellTravelService.entities.TravelBooking;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/wigelltravels/v1")
+public class TravelCustomerController {
+
+    @PostMapping("/booktrip")
+    public ResponseEntity<TravelBooking> bookTrip(@RequestBody TravelBooking travelBooking) { //TODO ta in och returnera DAO
+        return null;
+    }
+
+    @PutMapping("/canceltrip")
+    public ResponseEntity<TravelBooking> cancelTrip(@RequestBody TravelBooking travelBooking) { //TODO ta in och returnera DAO
+        return null;
+    }
+
+    @GetMapping("/mybookings")
+    public ResponseEntity<List<TravelBooking>> getMyBookings() { //TODO returnera DAO
+        return null;
+    }
+}
