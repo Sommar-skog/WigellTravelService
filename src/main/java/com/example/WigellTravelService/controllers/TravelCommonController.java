1284a0c695054dc937840c6228b4dfdb193b84f3
Sommar-skog <amandaaronsson@hotmail.com>
2025-09-18 09:29:46 +0200

    SecurityConfig - done. @PreAuth - done

--- src/main/java/com/example/WigellTravelService/controllers/TravelCommonController.java
+++ src/main/java/com/example/WigellTravelService/controllers/TravelCommonController.java
@@ -2,12 +2,14 @@ package com.example.WigellTravelService.controllers;
 
 import com.example.WigellTravelService.entities.TravelTrip;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
 @RestController
 @RequestMapping("/api/wigelltravels/v1")
 public class TravelCommonController {
--- src/main/java/com/example/WigellTravelService/controllers/TravelCustomerController.java
+++ src/main/java/com/example/WigellTravelService/controllers/TravelCustomerController.java
@@ -2,10 +2,12 @@ package com.example.WigellTravelService.controllers;
 
 import com.example.WigellTravelService.entities.TravelBooking;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@PreAuthorize("hasRole('USER')")
 @RestController
 @RequestMapping("/api/wigelltravels/v1")
 public class TravelCustomerController {
