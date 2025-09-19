5a2804ef89ac72d81a1e940b60b44e70d1830769
Sommar-skog <amandaaronsson@hotmail.com>
2025-09-19 10:32:06 +0200

    CustomerController done!

--- src/main/java/com/example/WigellTravelService/controllers/TravelCustomerController.java
+++ src/main/java/com/example/WigellTravelService/controllers/TravelCustomerController.java
@@ -2,7 +2,12 @@ package com.example.WigellTravelService.controllers;
 
 import com.example.WigellTravelService.dtos.CancelBookingDTO;
 import com.example.WigellTravelService.dtos.CreateBookingDTO;
+import com.example.WigellTravelService.dtos.TravelBookingDTO;
+import com.example.WigellTravelService.dtos.mappers.TravelBookingMapper;
 import com.example.WigellTravelService.entities.TravelBooking;
+import com.example.WigellTravelService.services.TravelBookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -15,18 +20,32 @@ import java.util.List;
 @RequestMapping("/api/wigelltravels/v1")
 public class TravelCustomerController {
 
+
+    private final TravelBookingService travelBookingService;
+
+    @Autowired
+    public TravelCustomerController(TravelBookingService travelBookingService) {
+        this.travelBookingService = travelBookingService;
+    }
+
+
     @PostMapping("/booktrip")
-    public ResponseEntity<TravelBooking> bookTrip(@RequestBody CreateBookingDTO createBookingDTO, Principal principal) { //TODO ta in och returnera DAO
-        return null;
+    public ResponseEntity<TravelBookingDTO> bookTrip(@RequestBody CreateBookingDTO createBookingDTO, Principal principal) {
+        return new ResponseEntity<>(TravelBookingMapper.toDTO(travelBookingService.bookTrip(createBookingDTO, principal)), HttpStatus.CREATED);
     }
 
     @PutMapping("/canceltrip")
-    public ResponseEntity<TravelBooking> cancelTrip(@RequestBody CancelBookingDTO cancelBookingDTO, Principal principal) { //TODO ta in och returnera DAO
-        return null;
+    public ResponseEntity<TravelBookingDTO> cancelTrip(@RequestBody CancelBookingDTO cancelBookingDTO, Principal principal) {
+        return ResponseEntity.ok(TravelBookingMapper.toDTO(travelBookingService.cancelTrip(cancelBookingDTO, principal)));
     }
 
     @GetMapping("/mybookings")
-    public ResponseEntity<List<TravelBooking>> getMyBookings(Principal principal) { //TODO returnera DAO
-        return null;
+    public ResponseEntity<List<TravelBookingDTO>> getMyBookings(Principal principal) {
+        List<TravelBookingDTO> dtoList = travelBookingService
+                .getMyBookings(principal)
+                .stream()
+                .map(TravelBookingMapper::toDTO)
+                .toList();
+        return ResponseEntity.ok(dtoList);
     }
 }
