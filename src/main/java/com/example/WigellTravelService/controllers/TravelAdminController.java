50033b5d3109a6ddb0bcf5c6f90ab16633079411
Sommar-skog <amandaaronsson@hotmail.com>
2025-09-19 09:18:00 +0200

    AdminController done!

--- src/main/java/com/example/WigellTravelService/controllers/TravelAdminController.java
+++ src/main/java/com/example/WigellTravelService/controllers/TravelAdminController.java
@@ -3,13 +3,14 @@ package com.example.WigellTravelService.controllers;
 
 import com.example.WigellTravelService.dtos.AddTravelPackageDTO;
 import com.example.WigellTravelService.dtos.TravelBookingDTO;
+import com.example.WigellTravelService.dtos.TravelPackageDTO;
 import com.example.WigellTravelService.dtos.UpdateTravelPackageDTO;
 import com.example.WigellTravelService.dtos.mappers.TravelBookingMapper;
-import com.example.WigellTravelService.entities.TravelBooking;
-import com.example.WigellTravelService.entities.TravelPackage;
+import com.example.WigellTravelService.dtos.mappers.TravelPackageMapper;
 import com.example.WigellTravelService.services.TravelBookingService;
 import com.example.WigellTravelService.services.TravelPackageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -22,10 +23,12 @@ import java.util.List;
 public class TravelAdminController {
 
     private final TravelBookingService travelBookingService;
+    private final TravelPackageService travelPackageService;
 
     @Autowired
-    public TravelAdminController(TravelBookingService travelBookingService) {
+    public TravelAdminController(TravelBookingService travelBookingService, TravelPackageService travelPackageService) {
         this.travelBookingService = travelBookingService;
+        this.travelPackageService = travelPackageService;
     }
 
     @GetMapping("/listcanceled")
@@ -59,17 +62,17 @@ public class TravelAdminController {
     }
 
     @PostMapping("/addtravel")
-    public ResponseEntity<TravelPackage> addTrip(@RequestBody AddTravelPackageDTO addTravelPackageDTO) { //TODO Ta in DAO returnera DAO
-        return null;
+    public ResponseEntity<TravelPackageDTO> addTrip(@RequestBody AddTravelPackageDTO addTravelPackageDTO) {
+        return new ResponseEntity<>(TravelPackageMapper.toDTO(travelPackageService.addTravelPackage(addTravelPackageDTO)), HttpStatus.CREATED);
     }
 
     @PutMapping("updatetravel")
-    public ResponseEntity<TravelPackage> updateTrip(@RequestBody UpdateTravelPackageDTO updateTravelPackageDTO) { //TODO ta in och returnera DAO
-        return null;
+    public ResponseEntity<TravelPackageDTO> updateTrip(@RequestBody UpdateTravelPackageDTO updateTravelPackageDTO) {
+        return ResponseEntity.ok(TravelPackageMapper.toDTO(travelPackageService.updateTravelPackage(updateTravelPackageDTO)));
     }
 
     @DeleteMapping("/removetravel/{id}")
     public ResponseEntity<String> removeTrip(@PathVariable Long id) {
-        return null;
+        return ResponseEntity.ok(travelPackageService.removeTravelPackage(id));
     }
 }
--- src/main/java/com/example/WigellTravelService/dtos/mappers/TravelPackageMapper.java
+++ src/main/java/com/example/WigellTravelService/dtos/mappers/TravelPackageMapper.java
@@ -1,11 +1,13 @@
 package com.example.WigellTravelService.dtos.mappers;
 
+import com.example.WigellTravelService.dtos.TravelPackageDTO;
 import com.example.WigellTravelService.entities.TravelPackage;
+
 
 public class TravelPackageMapper {
 
-    public static TravelPackage toDTO(TravelPackage travelPackage) {
-        TravelPackage dto = new TravelPackage();
+    public static TravelPackageDTO toDTO(TravelPackage travelPackage) {
+        TravelPackageDTO dto = new TravelPackageDTO();
         dto.setTravelPackageId(travelPackage.getTravelPackageId());
         dto.setDestination(travelPackage.getDestination());
         dto.setHotelName(travelPackage.getHotelName());
