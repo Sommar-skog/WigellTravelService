283f4a8170659391395d8c647f45e2a29abd26c0
Sommar-skog <amandaaronsson@hotmail.com>
2025-09-19 10:24:11 +0200

    CommonController done!

--- src/main/java/com/example/WigellTravelService/controllers/TravelCommonController.java
+++ src/main/java/com/example/WigellTravelService/controllers/TravelCommonController.java
@@ -1,6 +1,10 @@
 package com.example.WigellTravelService.controllers;
 
+import com.example.WigellTravelService.dtos.TravelPackageDTO;
+import com.example.WigellTravelService.dtos.mappers.TravelPackageMapper;
 import com.example.WigellTravelService.entities.TravelPackage;
+import com.example.WigellTravelService.services.TravelPackageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +18,21 @@ import java.util.List;
 @RequestMapping("/api/wigelltravels/v1")
 public class TravelCommonController {
 
+    private final TravelPackageService travelPackageService;
+
+    @Autowired
+    public TravelCommonController(TravelPackageService travelPackageService) {
+        this.travelPackageService = travelPackageService;
+    }
+
     @GetMapping("/travels")
-    public ResponseEntity<List<TravelPackage>> getAllTrips() { //TODO returnera DAO
-        return null;
+    public ResponseEntity<List<TravelPackageDTO>> getAllTravelPackages() {
+        List<TravelPackageDTO> dtoList = travelPackageService
+                .getAllTravelPackages()
+                .stream()
+                .map(TravelPackageMapper::toDTO)
+                .toList();
+        return ResponseEntity.ok(dtoList);
     }
 
 }
