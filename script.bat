ec9c542835208d9806a9d1e94c682f2db99d5a70
Sommar-skog <amandaaronsson@hotmail.com>
2025-09-24 09:50:27 +0200

    added script.bat everything works!

---
+++ script.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Stopping wigell-travel-service
+docker stop wigell-travel-service
+echo Deleting container wigell-travel-service
+docker rm wigell-travel-service
+echo Deleting image wigell-travel-service
+docker rmi wigell-travel-service
+echo Running mvn package
+call mvn package -DskipTests
+echo Creating image wigell-travel-service
+docker build -t wigell-travel-service .
+echo Creating and running container wigell-travel-service
+docker run -d -p 8585:8585 --name wigell-travel-service --network wigell-network wigell-travel-service
+echo Done!
