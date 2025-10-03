4440c7c29293239b2d1d2cca995322297841811d
Sommar-skog <amandaaronsson@hotmail.com>
2025-10-03 12:08:00 +0200

    added import.sql for .prod

---
+++ src/main/resources/import-prod.sql
@@ -0,0 +1,6 @@
+INSERT IGNORE INTO travel_customer(customer_name, username, password) VALUES
+                                                                    ('Alexander', 'alex','{noop}alex'),
+                                                                    ('Sara','sara','{noop}sara'),
+                                                                    ('Amanda', 'amanda', '{noop}amanda');
+
+
